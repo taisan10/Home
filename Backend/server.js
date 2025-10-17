f1644e94aa7c36398e3daa3e3c95db90ed9ecc36
taisan10 <tarunbansalsgnr@gmail.com>
2025-10-17 13:04:57 +0530

    first commit

--- Backend/server.js
+++ Backend/server.js
@@ -19,8 +19,8 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 // -------------------------
