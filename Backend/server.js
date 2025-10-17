0847a5bd336c1e789062292109b2eef10cfb4290
taisan10 <tarunbansalsgnr@gmail.com>
2025-10-17 12:30:28 +0530

    first commit

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import contactRoutes from "./routes/contact.js";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
 dotenv.config();
 
 const app = express();
@@ -10,5 +13,17 @@ app.use(express.json());
 
 app.use("/api", contactRoutes);
 
-app.listen(5000, () => console.log("Server running on http://localhost:5000"));
+// ---- Serve Frontend ----
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
+});
+// -------------------------
+
+app.listen(5000, "0.0.0.0", () => {
+  console.log("Server running on http://0.0.0.0:5000");
+});
--- Frontend/src/component/UI/Contactpage.jsx
+++ Frontend/src/component/UI/Contactpage.jsx
@@ -52,7 +52,7 @@ export default function ContactPage() {
     };
 
     try {
-      const res = await fetch(" http://localhost:5000/api/contact", {
+      const res = await fetch("http://72.60.173.145:5000/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
